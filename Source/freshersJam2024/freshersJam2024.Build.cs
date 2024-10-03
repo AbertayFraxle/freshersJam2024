fea18b5ba87a6dabe03db14d5c48737b9bed1c00
AbertayFraxle <91606018+AbertayFraxle@users.noreply.github.com>
2024-10-03 04:07:12 +0100

    WEBHOOK AND ALSO LAMP

--- Source/freshersJam2024/freshersJam2024.Build.cs
+++ Source/freshersJam2024/freshersJam2024.Build.cs
@@ -8,7 +8,7 @@ public class freshersJam2024 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "HTTP" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
